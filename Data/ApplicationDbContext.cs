80de07e5a941262f567d2148bc1e3c2495246b8a
Marius <121916282+S-Marius@users.noreply.github.com>
2024-08-16 13:26:41 +0200

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Document_Editor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Document_Editor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Document> Documents { get; set; }
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Document_Editor.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+    }
+}
